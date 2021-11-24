068d08065978a8e7dcf2035bd1a7d4296b52b0fa
abdulrazaqahmed <Abdulrazaq.Alsomali.dmm.k1@tuwaiq.edu.sa>
2021-11-24 17:47:04 +0300

    adapter PhotoRecyclerViewAdapter added

--- app/src/main/java/com/example/bookgallery/adapters/PhotoRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/bookgallery/adapters/PhotoRecyclerViewAdapter.kt
@@ -1,29 +1,64 @@
 package com.example.bookgallery.adapters
+
+import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import com.bumptech.glide.Glide
+import com.example.bookgallery.R
+import com.example.bookgallery.datamodels.Photo
 
 
-class PhotoRecyclerViewAdapter(private val list: List<Photo>) :
-  RecyclerView.Adapter<PhotoRecyclerViewAdapter.PhotoViewHolder>(){
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoRecyclerViewAdapter.PhotoViewHolder {
-      return PhotoViewHolder(
-          LayoutInflater.from(parent.context).inflate(
-              R.layout.item_layout,
-              parent,
-              false
-          )
-      )
-  }
-  override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
-  val item = list[position]
-            TODO("bind view with data")
-  }
-  override fun getItemCount(): Int {
-      return list.size
-  }
-  class PhotoViewHolder(itemView: View)
-  : RecyclerView.ViewHolder(itemView){
-  }
+class PhotoRecyclerViewAdapter(val context: Context) :
+    RecyclerView.Adapter<PhotoRecyclerViewAdapter.PhotoViewHolder>() {
+    val DIFF_CALL_BACK = object : DiffUtil.ItemCallback<Photo>(){
+        override fun areItemsTheSame(oldItem: Photo, newItem: Photo): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Photo, newItem: Photo): Boolean {
+            return oldItem == newItem
+        }
+
+
+    }
+
+    private val differ = AsyncListDiffer(this, DIFF_CALL_BACK)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): PhotoRecyclerViewAdapter.PhotoViewHolder {
+        return PhotoViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_layout,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: PhotoViewHolder, position: Int) {
+        val item = differ.currentList[position]
+        Glide.with(context)
+            .load(item.urlS)
+            .into(holder.imageView)
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+    fun submitList(list: List<Photo>) {
+        differ.submitList(list)
+    }
+    class PhotoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView : ImageView = itemView.findViewById(R.id.imageView)
+
+    }
 }
