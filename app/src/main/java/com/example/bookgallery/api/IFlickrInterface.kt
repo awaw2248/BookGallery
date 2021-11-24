685a88c08b8f71f57ec8e56e909752004225ff11
ali <alisaeedalmatrood@gmail.com>
2021-11-24 15:16:04 +0300

    Added Api interface, datamodels, ApiService object, and viewModel

---
+++ app/src/main/java/com/example/bookgallery/api/IFlickrInterface.kt
@@ -0,0 +1,18 @@
+package com.example.bookgallery.api
+
+import com.example.bookgallery.datamodels.PhotoModel
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface IFlickrInterface {
+    @GET("services/rest/?method=flickr.photos.search&api_key=83de65d98fa4f7eb77a44262cefb3612&format=json&extras=url_s&nojsoncallback=1")
+   suspend fun getPhotos(
+        @Query("lon") lon: Double,
+        @Query("lat") lat: Double,
+        @Query("page") page: Int
+    ): Response<PhotoModel>
+}
+
+
+
