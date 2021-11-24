2f867f629550d979617af30be0cbc95bb66fc4c5
ali <alisaeedalmatrood@gmail.com>
2021-11-24 16:22:12 +0300

    Completed photosViewModel

--- app/src/main/java/com/example/bookgallery/viewmodels/PhotosViewModel.kt
+++ app/src/main/java/com/example/bookgallery/viewmodels/PhotosViewModel.kt
@@ -1,19 +1,36 @@
 package com.example.bookgallery.viewmodels
 
-import androidx.lifecycle.LiveData
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.bookgallery.datamodels.Photo
+import com.example.bookgallery.repositories.FlickrApiService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class PhotosViewModel:ViewModel() {
+private const val TAG = "PhotosViewModel"
 
-    val photosLiveData = MutableLiveData<List<Photo>>()
-    fun getPhotos(lon:Double,lat:Double,page:Int) {
+class PhotosViewModel : ViewModel() {
+
+    private val photosLiveData = MutableLiveData<List<Photo>>()
+    private val errorLiveData = MutableLiveData<String>()
+    fun getPhotos(lon: Double, lat: Double, page: Int) {
         viewModelScope.launch(Dispatchers.IO) {
-
+            try {
+                val photoResponse = FlickrApiService.getPhotos(lon, lat, page)
+                if (photoResponse.isSuccessful) {
+                    photoResponse.body()?.let {
+                        photosLiveData.postValue(it.photos.photo)
+                    }
+                } else {
+                    Log.d(TAG, photoResponse.message())
+                    errorLiveData.postValue(photoResponse.message())
+                }
+            } catch (e: Exception) {
+                Log.d(TAG, e.message.toString())
+                errorLiveData.postValue(e.message)
+            }
         }
     }
 }
