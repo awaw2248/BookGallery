5cf0589298e85d2c596f7fc025d9a6997d1a4a56
Mustafa <Mustafa.Haassen.dmm.k1@tuwaiq.edu.sa>
2021-11-26 12:47:21 +0300

    "made some changes on the design"

--- app/src/main/java/com/example/bookgallery/MainActivity.kt
+++ app/src/main/java/com/example/bookgallery/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.bookgallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.SearchView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.navigation_menu, menu)
+
+
     }
 
 }
