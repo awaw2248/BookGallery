fc10cc145c24559c89b3e60480fc7c9c77325546
ali <alisaeedalmatrood@gmail.com>
2021-11-28 17:43:34 +0300

    implemented loading more photo pages

--- app/src/main/java/com/example/bookgallery/HomeFragment.kt
+++ app/src/main/java/com/example/bookgallery/HomeFragment.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.example.bookgallery.adapters.PhotoRecyclerViewAdapter
 import com.example.bookgallery.databinding.FragmentHomeBinding
 import com.example.bookgallery.datamodels.Photo
@@ -24,6 +25,7 @@ class HomeFragment : Fragment() {
     private lateinit var photosAdapter: PhotoRecyclerViewAdapter
     private lateinit var binding: FragmentHomeBinding
     val allPhotos = mutableListOf<Photo>()
+    var reachedEnd = false
 
     //===============================================
 
@@ -59,15 +61,31 @@ class HomeFragment : Fragment() {
 
         getCurrentLocation()
 
+        // implementing the code for retrieving the next page of photos when the user scrolls to the end of the list of photos.
+        binding.recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (!recyclerView.canScrollVertically(1) && newState == RecyclerView.SCROLL_STATE_IDLE && !viewModel.requested && viewModel.page <= viewModel.numberOfPages) {
+                    viewModel.getPhotos(longitude, latitude, viewModel.page)
+                    binding.newPageProgressBar.visibility = View.VISIBLE
+                }
+            }
+        })
+        //-----------------------------------------------------------------------------------------------//
+
+
     }
 
     private fun observers() {
         viewModel.photosLiveData.observe(viewLifecycleOwner, {
+            Log.d(TAG, "${viewModel.page}")
             Log.d(TAG, it.toString())
             it?.let {
+                Log.d(TAG, "INSIDE THE LET ")
                 binding.progressBar.visibility = View.INVISIBLE
+                binding.newPageProgressBar.visibility = View.GONE
                 allPhotos.addAll(it)
-                photosAdapter.submitList(it)
+                photosAdapter.submitList(allPhotos)
             }
         })
         viewModel.errorLiveData.observe(viewLifecycleOwner, {
@@ -82,7 +100,7 @@ class HomeFragment : Fragment() {
         })
     }
 
-    //-----------------------------------------------------------------------------------------------
+
     private fun getCurrentLocation() {
         if (ActivityCompat.checkSelfPermission(
                 requireActivity(),
@@ -95,7 +113,8 @@ class HomeFragment : Fragment() {
                     // getting the last known or current location
                     latitude = location.latitude
                     longitude = location.longitude
-                    viewModel.getPhotos(longitude, latitude, 1)
+                    val pageNumber = viewModel.page
+                    viewModel.getPhotos(longitude, latitude, pageNumber)
                     Log.d(TAG, "$latitude")
                     Log.d(TAG, "$longitude")
                 }
@@ -111,10 +130,12 @@ class HomeFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         //inflating the menu that has the search icon
         inflater.inflate(R.menu.navigation_menu, menu)
+        //---------------------------------------------------------------------------//
 
         //declaring the search item and assigning the search view to another variable to implement search functionality.
         val searchItem = menu.findItem(R.id.app_bar_search)
         val searchView = searchItem.actionView as SearchView
+        //---------------------------------------------------------------------------//
 
         // The code below to search for the text entered by the user.
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
--- app/src/main/java/com/example/bookgallery/viewmodels/PhotosViewModel.kt
+++ app/src/main/java/com/example/bookgallery/viewmodels/PhotosViewModel.kt
@@ -9,6 +9,7 @@ import com.example.bookgallery.repositories.FlickrApiService
 import com.example.bookgallery.repositories.RoomServiceRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 private const val TAG = "PhotosViewModel"
 
@@ -19,28 +20,39 @@ class PhotosViewModel : ViewModel() {
     val photosLiveData = MutableLiveData<List<Photo>>()
     val errorLiveData = MutableLiveData<String>()
     val permission = MutableLiveData<Boolean>()
+    var numberOfPages: Int = 1
+    var requested = false
+    var page = 1
     fun getPhotos(lon: Double, lat: Double, page: Int) {
-        viewModelScope.launch(Dispatchers.IO) {
-            try {
-                val photoResponse = FlickrApiService.getPhotos(lon, lat, page)
-                if (photoResponse.isSuccessful) {
-                    photoResponse.body()?.let {
-                        val listOfPhotos = it.photos.photo
-                        if (listOfPhotos.isNotEmpty()) {
-                            Log.d(TAG, it.photos.photo.toString())
-                            photosLiveData.postValue(listOfPhotos)
-                            databaseRepo.insertPhotos(listOfPhotos)
+        if (page <= numberOfPages) {
+            viewModelScope.launch(Dispatchers.IO) {
+                requested = true
+                try {
+                    val photoResponse = FlickrApiService.getPhotos(lon, lat, page)
+                    if (photoResponse.isSuccessful) {
+                        photoResponse.body()?.let {
+                            val listOfPhotos = it.photos.photo
+                            numberOfPages = it.photos.pages
+                            Log.d(TAG,"number of pages: $numberOfPages")
+                            if (listOfPhotos.isNotEmpty()) {
+                                Log.d(TAG, it.photos.photo.toString())
+                                photosLiveData.postValue(listOfPhotos)
+                                databaseRepo.insertPhotos(listOfPhotos)
+                                this@PhotosViewModel.page += 1
+                                requested = false
+                                Log.d(TAG,"${this@PhotosViewModel.page}")
+                            }
                         }
+                    } else {
+                        Log.d(TAG, photoResponse.message())
+                        errorLiveData.postValue("Error while retrieving data")
+                        photosLiveData.postValue(databaseRepo.getPhoto())
                     }
-                } else {
-                    Log.d(TAG, photoResponse.message())
-                    errorLiveData.postValue("Error while retrieving data")
+                } catch (e: Exception) {
+                    Log.d(TAG, e.message.toString())
+                    errorLiveData.postValue("Could not connect to server")
                     photosLiveData.postValue(databaseRepo.getPhoto())
                 }
-            } catch (e: Exception) {
-                Log.d(TAG, e.message.toString())
-                errorLiveData.postValue("Could not connect to server")
-                photosLiveData.postValue(databaseRepo.getPhoto())
             }
         }
     }
